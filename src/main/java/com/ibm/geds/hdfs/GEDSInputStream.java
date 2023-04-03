aaad8c759eb8cd470275b221541ec7bc82aa7504
ePezhman <epezhman@gmail.com>
2023-04-03 14:19:05 +0200

    small bug fixes

--- src/main/java/com/ibm/geds/hdfs/GEDSInputStream.java
+++ src/main/java/com/ibm/geds/hdfs/GEDSInputStream.java
@@ -17,7 +17,7 @@ import static java.lang.Math.min;
 public class GEDSInputStream extends FSInputStream {
 
     private final GEDSFile file;
-    private final long lfileSize;
+    private final long fileSize;
 
     public GEDSInputStream(GEDSFile file) {
         this.file = file;
